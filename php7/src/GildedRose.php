e8e13885beb12b2fc2d7c360e1f13aba9010af41
wangwayne681 <wangwayne681@gmail.com>
2020-07-31 14:50:57 +0800

    繼續用 switch 改寫流程

--- php7/src/GildedRose.php
+++ php7/src/GildedRose.php
@@ -14,16 +14,16 @@ final class GildedRose {
     public function updateQuality()
     {
         foreach ($this->items as $item) {
-            if ($item->name == 'Sulfuras, Hand of Ragnaros') {
-            } else {
-                $item->sell_in = $item->sell_in - 1;
-            }
-
             switch ($item->name) {
                 case 'Aged Brie':
+                    $item->sell_in = $item->sell_in - 1;
                     $item->quality = $this->addQuality($item->quality);
+                    if ($item->sell_in < 0) {
+                        $item->quality = $this->addQuality($item->quality);
+                    }
                     break;
                 case 'Backstage passes to a TAFKAL80ETC concert':
+                    $item->sell_in = $item->sell_in - 1;
                     $item->quality = $this->addQuality($item->quality);
                     if ($item->sell_in < 10) {
                         $item->quality = $this->addQuality($item->quality);
@@ -31,27 +31,19 @@ final class GildedRose {
                     if ($item->sell_in < 5) {
                         $item->quality = $this->addQuality($item->quality);
                     }
+                    if ($item->sell_in < 0) {
+                        $item->quality = 0;
+                    }
                     break;
                 case 'Sulfuras, Hand of Ragnaros':
                     break;
                 default:
+                    $item->sell_in = $item->sell_in - 1;
                     $item->quality = $this->decreaseQuality($item->quality);
+                    if ($item->sell_in < 0) {
+                        $item->quality = $this->decreaseQuality($item->quality);
+                    }
                     break;
-            }
-            
-            if ($item->sell_in < 0) {
-                switch ($item->name) {
-                    case 'Aged Brie':
-                        $item->quality = $this->addQuality($item->quality);
-                        break;
-                    case 'Backstage passes to a TAFKAL80ETC concert':
-                        $item->quality = 0;
-                        break;
-                    case 'Sulfuras, Hand of Ragnaros':
-                        break;
-                    default:
-                    $item->quality = $this->decreaseQuality($item->quality);
-                }
             }
         }
     }
@@ -71,7 +63,7 @@ final class GildedRose {
     private function decreaseQuality($quality)
     {
         if ($quality > 0) {
-            $quality = $quality - 1;
+            return $quality - 1;
         }
         return $quality;
     }
